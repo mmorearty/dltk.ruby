b832aeb92a5003fe03a30e5e2eedccbee46c8eee
dkovalev <dkovalev>
2007-03-19 12:28:07 +0000

    Tcl code templates added

--- plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/templates/RubyTemplateCompletionProcessor.java
+++ plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/templates/RubyTemplateCompletionProcessor.java
@@ -1,17 +1,12 @@
 package org.eclipse.dltk.ruby.internal.ui.templates;
 
-import org.eclipse.dltk.ruby.internal.ui.RubyTemplateAccess;
 import org.eclipse.dltk.ui.templates.ScriptTempalteCompletionProcessor;
-import org.eclipse.dltk.ui.templates.ScriptTemplateContext;
-import org.eclipse.dltk.ui.templates.ScriptTemplateContextType;
 import org.eclipse.dltk.ui.text.completion.ScriptContentAssistInvocationContext;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.templates.DocumentTemplateContext;
 import org.eclipse.jface.text.templates.Template;
-import org.eclipse.jface.text.templates.TemplateContext;
 import org.eclipse.jface.text.templates.TemplateContextType;
 import org.eclipse.swt.graphics.Image;
 
@@ -69,64 +64,4 @@ public class RubyTemplateCompletionProcessor extends
 	protected Image getImage(Template template) {
 		return null;
 	}
-
-	protected TemplateContext createContext(ITextViewer viewer, IRegion region) {
-		TemplateContextType contextType = getContextType(viewer, region);
-		if (contextType instanceof ScriptTemplateContextType) {
-			IDocument document = viewer.getDocument();
-																	
-			
-			return ((ScriptTemplateContextType) contextType).createContext(
-					document, region.getOffset(), region.getLength(),
-					getContext().getSourceModule());
-		} else {
-			return super.createContext(viewer, region);
-		}
-	}
-
-	// protected TemplateContext createContext(final ITextViewer viewer, final
-	// IRegion region) {
-	// TemplateContextType contextType = getContextType(viewer, region);
-	// if (contextType != null) {
-	// IDocument document= viewer.getDocument();
-	// String indent = "";
-	// PySelection selection = new PySelection(document,
-	// viewer.getTextWidget().getSelection().x);
-	// indent = selection.getIndentationFromLine();
-	//            
-	// final String indentTo = indent;
-	//            
-	// return new DocumentTemplateContext(contextType, document,
-	// region.getOffset(), region.getLength()){
-	// public TemplateBuffer evaluate(Template template) throws
-	// BadLocationException, TemplateException {
-	// if (!canEvaluate(template))
-	// return null;
-	//
-	// String pattern = template.getPattern();
-	// List splitted = StringUtils.splitInLines(pattern);
-	// if(splitted.size() > 1 && indentTo != null && indentTo.length() > 0){
-	// StringBuffer buffer = new StringBuffer(splitted.get(0));
-	// for (int i=1; i<splitted.size();i++) { //we don't want to get the first
-	// line
-	// buffer.append(indentTo);
-	// buffer.append(splitted.get(i));
-	// }
-	// //just to change the pattern...
-	// template = new Template(template.getName(), template.getDescription(),
-	// template.getContextTypeId(), buffer.toString(),
-	// template.isAutoInsertable());
-	// }
-	//                    
-	// TemplateTranslator translator= new TemplateTranslator();
-	// TemplateBuffer templateBuffer= translator.translate(template);
-	//
-	// getContextType().resolve(templateBuffer, this);
-	//
-	// return templateBuffer;
-	// }
-	// };
-	// }
-	// return null;
-	// }
 }
