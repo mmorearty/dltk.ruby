4c40cc970100bb53a065323929702278e6301000
jgangemi <jgangemi>
2007-12-16 23:08:20 +0000

    - added preference initializer - converted to active state, basic debugger to use shared property/preference page - enabled property pages, however project specific properties are ignored (for now)

--- plugins/org.eclipse.dltk.ruby.basicdebugger/src/org/eclipse/dltk/ruby/basicdebugger/preferences/RubyBasicDebuggerPreferencePage.java
+++ plugins/org.eclipse.dltk.ruby.basicdebugger/src/org/eclipse/dltk/ruby/basicdebugger/preferences/RubyBasicDebuggerPreferencePage.java
@@ -4,46 +4,88 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- 
- *******************************************************************************/
+ ******************************************************************************/
 
 package org.eclipse.dltk.ruby.basicdebugger.preferences;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.dltk.ruby.basicdebugger.RubyBasicDebuggerPlugin;
-import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPreferencePage;
-import org.eclipse.dltk.ui.preferences.IPreferenceConfigurationBlock;
-import org.eclipse.dltk.ui.preferences.ImprovedAbstractConfigurationBlock;
-import org.eclipse.dltk.ui.preferences.OverlayPreferenceStore;
+import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage;
+import org.eclipse.dltk.ui.preferences.AbstractOptionsBlock;
+import org.eclipse.dltk.ui.preferences.PreferenceKey;
+import org.eclipse.dltk.ui.util.IStatusChangeListener;
 import org.eclipse.dltk.ui.util.SWTFactory;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 
 public class RubyBasicDebuggerPreferencePage extends
-		AbstractConfigurationBlockPreferencePage {
-	public static final String PAGE_ID = "org.eclipse.dltk.ruby.preferences.debug.basicdebugger";
+		AbstractConfigurationBlockPropertyAndPreferencePage {
+	
+	private static String PREFERENCE_PAGE_ID = "org.eclipse.dltk.ruby.preferences.debug.engines.basicdebugger";
+	private static String PROPERTY_PAGE_ID = "org.eclipse.dltk.ruby.propertyPage.debug.engines.basicdebugger";
 
-	protected IPreferenceConfigurationBlock createConfigurationBlock(
-			OverlayPreferenceStore store) {
-		return new ImprovedAbstractConfigurationBlock(store, this) {
-			public Control createControl(Composite parent) {
+	/*
+	 * @see org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage#createOptionsBlock(org.eclipse.dltk.ui.util.IStatusChangeListener,
+	 *      org.eclipse.core.resources.IProject,
+	 *      org.eclipse.ui.preferences.IWorkbenchPreferenceContainer)
+	 */
+	protected AbstractOptionsBlock createOptionsBlock(
+			IStatusChangeListener newStatusChangedListener, IProject project,
+			IWorkbenchPreferenceContainer container) {
+		return new AbstractOptionsBlock(newStatusChangedListener, project,
+				new PreferenceKey[] {}, container) {
+
+			protected Control createOptionsBlock(Composite parent) {
 				Composite composite = SWTFactory.createComposite(parent, parent
 						.getFont(), 1, 1, GridData.FILL);
-				SWTFactory.createLabel(composite, "There are no settings now.", 1);
+				SWTFactory.createLabel(composite, PreferenceMessages.NoSettingsAvailable,
+						1);
 				return composite;
 			}
 		};
 	}
 
+	/*
+	 * @see org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage#getHelpId()
+	 */
 	protected String getHelpId() {
 		return null;
 	}
 
+	/*
+	 * @see org.eclipse.dltk.internal.ui.preferences.PropertyAndPreferencePage#getPreferencePageId()
+	 */
+	protected String getPreferencePageId() {
+		return PREFERENCE_PAGE_ID;
+	}
+
+	/*
+	 * @see org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage#getProjectHelpId()
+	 */
+	protected String getProjectHelpId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.dltk.internal.ui.preferences.PropertyAndPreferencePage#getPropertyPageId()
+	 */
+	protected String getPropertyPageId() {
+		return PROPERTY_PAGE_ID;
+	}
+
+	/*
+	 * @see org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage#setDescription()
+	 */
 	protected void setDescription() {
 		setDescription(PreferenceMessages.PreferencesDescription);
 	}
 
+	/*
+	 * @see org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage#setPreferenceStore()
+	 */
 	protected void setPreferenceStore() {
 		setPreferenceStore(RubyBasicDebuggerPlugin.getDefault()
 				.getPreferenceStore());
