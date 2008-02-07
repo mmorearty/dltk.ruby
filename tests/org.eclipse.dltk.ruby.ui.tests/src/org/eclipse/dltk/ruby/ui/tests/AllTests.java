0a107a484654ea2702908156f9bb00b16b8c5081
asobolev <asobolev>
2008-02-07 12:28:30 +0000

    * patch applied

--- tests/org.eclipse.dltk.ruby.ui.tests/src/org/eclipse/dltk/ruby/ui/tests/AllTests.java
+++ tests/org.eclipse.dltk.ruby.ui.tests/src/org/eclipse/dltk/ruby/ui/tests/AllTests.java
@@ -3,7 +3,6 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.dltk.ruby.ui.tests.indenting.RubyAutoIndentStrategyTest;
-import org.eclipse.dltk.ruby.ui.tests.text.ContextTest;
 import org.eclipse.dltk.ruby.ui.tests.text.PartitioningTest;
 import org.eclipse.dltk.ruby.ui.tests.text.indenting.IndentingTest;
 
@@ -15,7 +14,6 @@ public class AllTests {
 		suite.addTestSuite(RubyAutoIndentStrategyTest.class);
 		suite.addTestSuite(PartitioningTest.class);
 		suite.addTestSuite(IndentingTest.class);
-		suite.addTestSuite(ContextTest.class);
 		// $JUnit-END$
 		return suite;
 	}
