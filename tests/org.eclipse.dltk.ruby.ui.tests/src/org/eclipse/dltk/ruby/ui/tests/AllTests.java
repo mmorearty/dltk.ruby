a8f37c8285d8d581ed1dfe9598c085e5270bd376
ssanders <ssanders>
2008-04-01 01:55:08 +0000

    Refactored tests to avoid collisions when run as a super suite, corrected line-endings in Ruby files.

--- tests/org.eclipse.dltk.ruby.ui.tests/src/org/eclipse/dltk/ruby/ui/tests/AllTests.java
+++ tests/org.eclipse.dltk.ruby.ui.tests/src/org/eclipse/dltk/ruby/ui/tests/AllTests.java
@@ -9,7 +9,7 @@ import org.eclipse.dltk.ruby.ui.tests.text.indenting.IndentingTest;
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for default package");
+		TestSuite suite = new TestSuite("Test for org.eclipse.dltk.ruby.ui.tests");
 		// $JUnit-BEGIN$
 		suite.addTestSuite(RubyAutoIndentStrategyTest.class);
 		suite.addTestSuite(PartitioningTest.class);
