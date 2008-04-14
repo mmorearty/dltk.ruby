7cdea928698d3cd4238120fc4415dd3c907f7537
asobolev <asobolev>
2008-04-14 06:39:22 +0000

    *fixes

--- plugins/org.eclipse.dltk.ruby.launching/src/org/eclipse/dltk/ruby/internal/launching/RubyGenericInstallType.java
+++ plugins/org.eclipse.dltk.ruby.launching/src/org/eclipse/dltk/ruby/internal/launching/RubyGenericInstallType.java
@@ -23,7 +23,8 @@ public class RubyGenericInstallType extends AbstractInterpreterInstallType {
 
 	private static final String INSTALL_TYPE_NAME = "Generic Ruby"; //$NON-NLS-1$
 
-	private static final String[] INTERPRETER_NAMES = { "ruby", "rubyw" }; //$NON-NLS-1$ //$NON-NLS-2$
+	private static final String[] INTERPRETER_NAMES = {
+			"ruby", "rubyw", "ruby18", "ruby19" }; //$NON-NLS-1$ //$NON-NLS-2$
 
 	public String getNatureId() {
 		return RubyNature.NATURE_ID;
@@ -46,8 +47,8 @@ public class RubyGenericInstallType extends AbstractInterpreterInstallType {
 	}
 
 	protected IPath createPathFile(IDeployment deployment) throws IOException {
-		return deployment.add(
-				RubyLaunchingPlugin.getDefault().getBundle(), "scripts/path.rb"); //$NON-NLS-1$
+		return deployment.add(RubyLaunchingPlugin.getDefault().getBundle(),
+				"scripts/path.rb"); //$NON-NLS-1$
 	}
 
 	protected String getBuildPathDelimeter() {
