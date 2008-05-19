3fe58b180e00b154dd715078a63877145621b664
asobolev <asobolev>
2008-05-19 07:59:53 +0000

    * todo's parser fixed * some label fixes * correct interpreter dialog table columns size

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubySourceElementParser.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubySourceElementParser.java
@@ -5,15 +5,20 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- 
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation
+ *     xored software, Inc. - todo task parser added (Alex Panchenko)
  *******************************************************************************/
 package org.eclipse.dltk.ruby.internal.parser;
 
+import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.compiler.SourceElementRequestVisitor;
 import org.eclipse.dltk.compiler.task.ITaskReporter;
+import org.eclipse.dltk.compiler.task.TodoTaskAstParser;
 import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
-import org.eclipse.dltk.compiler.task.TodoTaskSimpleParser;
 import org.eclipse.dltk.core.AbstractSourceElementParser;
+import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.dltk.core.SourceParserUtil;
 import org.eclipse.dltk.core.ISourceModuleInfoCache.ISourceModuleInfo;
 import org.eclipse.dltk.ruby.core.RubyNature;
 import org.eclipse.dltk.ruby.core.RubyPlugin;
@@ -37,23 +42,32 @@ public class RubySourceElementParser extends AbstractSourceElementParser {
 
 	public void parseSourceModule(char[] contents, ISourceModuleInfo astCache,
 			char[] filename) {
-		super.parseSourceModule(contents, astCache, filename);
+		ModuleDeclaration moduleDeclaration = SourceParserUtil
+				.getModuleDeclaration(filename, contents, getNatureId(),
+						getProblemReporter(), astCache);
+		final SourceElementRequestVisitor requestor = createVisitor();
+		try {
+			moduleDeclaration.traverse(requestor);
+		} catch (Exception e) {
+			DLTKCore.error("Unexpected error", e); //$NON-NLS-1$
+		}
 		if (getProblemReporter() != null) {
 			final ITaskReporter taskReporter = (ITaskReporter) getProblemReporter()
 					.getAdapter(ITaskReporter.class);
 			if (taskReporter != null) {
 				taskReporter.clearTasks();
-				parseTasks(taskReporter, contents);
+				parseTasks(taskReporter, contents, moduleDeclaration);
 			}
 		}
 	}
 
-	protected void parseTasks(ITaskReporter taskReporter, char[] content) {
+	protected void parseTasks(ITaskReporter taskReporter, char[] content,
+			ModuleDeclaration moduleDeclaration) {
 		final TodoTaskPreferences preferences = new TodoTaskPreferences(
 				RubyPlugin.getDefault().getPluginPreferences());
 		if (preferences.isEnabled()) {
-			final TodoTaskSimpleParser taskParser = new TodoTaskSimpleParser(
-					taskReporter, preferences);
+			final TodoTaskAstParser taskParser = new TodoTaskAstParser(
+					taskReporter, preferences, moduleDeclaration);
 			if (taskParser.isValid()) {
 				taskParser.parse(content);
 			}
