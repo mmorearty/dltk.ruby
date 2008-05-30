257c934c0e355bcac0445b7c3f7ad9969c7403f8
apanchenk <apanchenk>
2008-05-30 13:16:01 +0000

    filename:linenumber are hyperlinked in ruby console - completed initial code

--- tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/AllTests.java
+++ tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/AllTests.java
@@ -3,6 +3,7 @@ package org.eclipse.dltk.ruby.debug.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.dltk.ruby.debug.tests.console.RubyFilenameLinenumberTests;
 import org.eclipse.dltk.ruby.debug.tests.launching.RubyLaunchingTests;
 
 public class AllTests {
@@ -12,6 +13,7 @@ public class AllTests {
 
 		// Launching
 		suite.addTest(RubyLaunchingTests.suite());
+		suite.addTest(RubyFilenameLinenumberTests.suite());
 		// $JUnit-END$
 		return suite;
 	}
---
+++ tests/org.eclipse.dltk.ruby.debug.tests/src/org/eclipse/dltk/ruby/debug/tests/console/RubyFilenameLinenumberTests.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2008 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.ruby.debug.tests.console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import junit.framework.Test;
+
+import org.eclipse.dltk.core.tests.model.SuiteOfTestCases;
+import org.eclipse.dltk.ruby.debug.RubyFilenameLinenumberResolver;
+
+public class RubyFilenameLinenumberTests extends SuiteOfTestCases {
+
+	public static Test suite() {
+		return new Suite(RubyFilenameLinenumberTests.class);
+	}
+
+	private final Pattern pattern;
+
+	public RubyFilenameLinenumberTests(String name) {
+		super(name);
+		RubyFilenameLinenumberResolver resolver = new RubyFilenameLinenumberResolver();
+		pattern = Pattern.compile(resolver.resolveValue(null, null));
+	}
+
+	public void testSimple() throws Exception {
+		assertTrue(pattern.matcher("script.rb:2").matches());
+	}
+
+	public void testRoot() throws Exception {
+		assertTrue(pattern.matcher("/script.rb:2").matches());
+	}
+
+	public void testFullWindows() throws Exception {
+		assertTrue(pattern.matcher("C:/runtime/A/script.rb:2").matches());
+	}
+
+	public void testFullUnix() throws Exception {
+		assertTrue(pattern.matcher("/home/user/runtime/A/script.rb:2")
+				.matches());
+	}
+
+	public void testFrom() throws Exception {
+		Matcher m = pattern
+				.matcher("from c:/ruby/lib/ruby/site_ruby/1.8/rubygems.rb:61:in `require_gem'");
+		assertTrue(m.find());
+		assertEquals(2, m.groupCount());
+		assertEquals("61", m.group(2));
+		final String filename = m.group(1);
+		assertTrue(filename.startsWith("c:/"));
+		assertTrue(filename.endsWith("rubygems.rb"));
+	}
+
+	public void testNegativeMatch() {
+//		Matcher m = pattern
+//				.matcher("C:0:Warning: require_gem is obsolete.  Use gem instead.");
+//		assertFalse(m.find());
+	}
+
+}
