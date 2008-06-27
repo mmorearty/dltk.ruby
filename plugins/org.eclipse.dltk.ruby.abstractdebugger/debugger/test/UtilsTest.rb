f9a8b43f5ff88f4d81bb11b79d63360bcd5e5685
apanchenk <apanchenk>
2008-06-27 09:40:38 +0000

    test path_to_uri and uri_to_path

--- plugins/org.eclipse.dltk.ruby.abstractdebugger/debugger/test/UtilsTest.rb
+++ plugins/org.eclipse.dltk.ruby.abstractdebugger/debugger/test/UtilsTest.rb
@@ -1,3 +1,16 @@
+if $0 == __FILE__
+  require 'test/unit/testsuite'
+  $LOAD_PATH << '..'
+
+  # disable logging
+  require 'common/Logger'
+  require 'common/NullLogManager'
+  require 'common/StdoutLogManager'
+  module XoredDebugger
+    Logger.setup(StdoutLogManager.new)
+  end
+end
+
 require 'test/unit'
 require 'dbgp/Utils.rb'
 
@@ -5,16 +18,21 @@ module XoredDebugger
   class UtilsTest < Test::Unit::TestCase
     include XoredDebuggerUtils
 
+    def assert_convertion(uri,file)
+      assert_equal uri, path_to_uri(file)
+      assert_equal file, uri_to_path(uri)
+    end
+
     def test_pathToURI_windows
-      assert_equal 'file:///c:/windows', path_to_uri('c:/windows')
+      assert_convertion 'file:///c:/windows', 'c:/windows'
     end
 
     def test_pathToURI_unix
-      assert_equal 'file:///home/user', path_to_uri('/home/user')
+      assert_convertion 'file:///home/user', '/home/user'
     end
 
     def test_pathToURI_special_chars
-      assert_equal 'file:///home%5B%5D', path_to_uri('/home[]')
+      assert_convertion 'file:///home%5B%5D', '/home[]'
     end
 
   end
