3802c2dc2bdf1a5228d535c88040620aace66193
apanchenk <apanchenk>
2008-07-02 10:37:52 +0000

    equalsIgnoreCase() for strings and arrays

--- plugins/org.eclipse.dltk.ruby.debug/src/org/eclipse/dltk/ruby/debug/RubyTypeFactory.java
+++ plugins/org.eclipse.dltk.ruby.debug/src/org/eclipse/dltk/ruby/debug/RubyTypeFactory.java
@@ -10,35 +10,28 @@ import org.eclipse.dltk.debug.core.model.StringScriptType;
 
 public class RubyTypeFactory implements IScriptTypeFactory {
 
-	private static final String STRING2 = "String"; //$NON-NLS-1$
-
 	private static final String[] atomicTypes = { "Bignum", "FalseClass", //$NON-NLS-1$  //$NON-NLS-2$
 			"Fixnum", "Float", "Integer", "NilClass", "Numeric", "Range", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
 			"Regexp", "Symbol", "TrueClass" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 	public IScriptType buildType(String type) {
-		if (STRING.equals(type) || STRING2.equals(type)) {
+		if (STRING.equalsIgnoreCase(type)) {
 			return new StringScriptType(type);
 		}
-
+		if (ARRAY.equalsIgnoreCase(type)) {
+			return new ArrayScriptType();
+		}
+		if (HASH.equals(type)) {
+			return new HashScriptType();
+		}
 		for (int i = 0; i < atomicTypes.length; ++i) {
 			if (atomicTypes[i].equals(type)) {
 				return new AtomicScriptType(type);
 			}
 		}
-
-		if (ARRAY.equals(type)) {
-			return new ArrayScriptType();
-		}
-
-		if (HASH.equals(type)) {
-			return new HashScriptType();
-		}
-
 		if (RubySetScriptType.SET.equals(type)) {
 			return new RubySetScriptType();
 		}
-
 		return new ComplexScriptType(type);
 	}
 }
