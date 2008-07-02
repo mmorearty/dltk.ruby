83f5aeb1da2e1ebca6d656f21e8297ac7e2db60d
apanchenk <apanchenk>
2008-07-02 05:41:02 +0000

    final & NON-NLS comment

--- plugins/org.eclipse.dltk.ruby.debug/src/org/eclipse/dltk/ruby/debug/RubySetScriptType.java
+++ plugins/org.eclipse.dltk.ruby.debug/src/org/eclipse/dltk/ruby/debug/RubySetScriptType.java
@@ -3,9 +3,9 @@ package org.eclipse.dltk.ruby.debug;
 import org.eclipse.dltk.debug.core.model.CollectionScriptType;
 
 public class RubySetScriptType extends CollectionScriptType {
-	
-	protected static String SET = "set";
-	
+
+	protected static final String SET = "set"; //$NON-NLS-1$
+
 	public RubySetScriptType() {
 		super(SET);
 	}
