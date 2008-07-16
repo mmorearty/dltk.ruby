563c70b31a8895607bc868fd74a44b81469fe06e
apanchenk <apanchenk>
2008-07-16 13:38:15 +0000

    TODO task parsing via new API

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubySourceElementParser.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubySourceElementParser.java
@@ -11,17 +11,9 @@
  *******************************************************************************/
 package org.eclipse.dltk.ruby.internal.parser;
 
-import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.compiler.SourceElementRequestVisitor;
-import org.eclipse.dltk.compiler.task.ITaskReporter;
-import org.eclipse.dltk.compiler.task.TodoTaskAstParser;
-import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
 import org.eclipse.dltk.core.AbstractSourceElementParser;
-import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.SourceParserUtil;
-import org.eclipse.dltk.core.ISourceModuleInfoCache.ISourceModuleInfo;
 import org.eclipse.dltk.ruby.core.RubyNature;
-import org.eclipse.dltk.ruby.core.RubyPlugin;
 import org.eclipse.dltk.ruby.internal.parser.visitors.RubySourceElementRequestor;
 
 public class RubySourceElementParser extends AbstractSourceElementParser {
@@ -38,40 +30,6 @@ public class RubySourceElementParser extends AbstractSourceElementParser {
 	 */
 	protected String getNatureId() {
 		return RubyNature.NATURE_ID;
-	}
-
-	public void parseSourceModule(char[] contents, ISourceModuleInfo astCache,
-			char[] filename) {
-		ModuleDeclaration moduleDeclaration = SourceParserUtil
-				.getModuleDeclaration(filename, contents, getNatureId(),
-						getProblemReporter(), astCache);
-		final SourceElementRequestVisitor requestor = createVisitor();
-		try {
-			moduleDeclaration.traverse(requestor);
-		} catch (Exception e) {
-			DLTKCore.error("Unexpected error", e); //$NON-NLS-1$
-		}
-		if (getProblemReporter() != null) {
-			final ITaskReporter taskReporter = (ITaskReporter) getProblemReporter()
-					.getAdapter(ITaskReporter.class);
-			if (taskReporter != null) {
-				taskReporter.clearTasks();
-				parseTasks(taskReporter, contents, moduleDeclaration);
-			}
-		}
-	}
-
-	protected void parseTasks(ITaskReporter taskReporter, char[] content,
-			ModuleDeclaration moduleDeclaration) {
-		final TodoTaskPreferences preferences = new TodoTaskPreferences(
-				RubyPlugin.getDefault().getPluginPreferences());
-		if (preferences.isEnabled()) {
-			final TodoTaskAstParser taskParser = new TodoTaskAstParser(
-					taskReporter, preferences, moduleDeclaration);
-			if (taskParser.isValid()) {
-				taskParser.parse(content);
-			}
-		}
 	}
 
 }
---
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubyTodoParserType.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2008 xored software, Inc.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Alex Panchenko)
+ *******************************************************************************/
+package org.eclipse.dltk.ruby.internal.parser;
+
+import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
+import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.ruby.core.RubyNature;
+import org.eclipse.dltk.ruby.core.RubyPlugin;
+import org.eclipse.dltk.validators.core.AbstractBuildParticipantType;
+import org.eclipse.dltk.validators.core.IBuildParticipant;
+
+public class RubyTodoParserType extends AbstractBuildParticipantType {
+
+	private static final String ID = "org.eclipse.dltk.ruby.todo"; //$NON-NLS-1$
+	private static final String NAME = "Ruby TODO task parser"; //$NON-NLS-1$
+
+	public RubyTodoParserType() {
+		super(ID, NAME);
+	}
+
+	protected IBuildParticipant createBuildParticipant(IScriptProject project) {
+		return new RubyTodoTaskAstParser(new TodoTaskPreferences(RubyPlugin
+				.getDefault().getPluginPreferences()));
+	}
+
+	public String getNature() {
+		return RubyNature.NATURE_ID;
+	}
+
+}
