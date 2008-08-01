b12a7a37b75ebf0eecf718ecbef603231ac237b6
apanchenk <apanchenk>
2008-08-01 08:06:16 +0000

    toString() writes exact text output with control characters escaped

--- plugins/org.eclipse.dltk.ruby.formatter/src/org/eclipse/dltk/formatter/nodes/FormatterTextNode.java
+++ plugins/org.eclipse.dltk.ruby.formatter/src/org/eclipse/dltk/formatter/nodes/FormatterTextNode.java
@@ -11,6 +11,8 @@
  *******************************************************************************/
 package org.eclipse.dltk.formatter.nodes;
 
+import java.io.StringWriter;
+
 public class FormatterTextNode extends AbstractFormatterNode implements
 		IFormatterTextNode {
 
@@ -51,7 +53,71 @@ public class FormatterTextNode extends AbstractFormatterNode implements
 	}
 
 	public String toString() {
-		return getText().trim();
+		final StringWriter w = new StringWriter();
+		escapeJavaStyleString(w, getText());
+		return w.toString();
+	}
+
+	private static void escapeJavaStyleString(StringWriter out, String str) {
+		if (str == null) {
+			return;
+		}
+		int sz;
+		sz = str.length();
+		for (int i = 0; i < sz; i++) {
+			char ch = str.charAt(i);
+			if (ch > 0xfff) {
+				out.write("\\u" + hex(ch));
+			} else if (ch > 0xff) {
+				out.write("\\u0" + hex(ch));
+			} else if (ch > 0x7f) {
+				out.write("\\u00" + hex(ch));
+			} else if (ch < 32) {
+				switch (ch) {
+				case '\b':
+					out.write('\\');
+					out.write('b');
+					break;
+				case '\n':
+					out.write('\\');
+					out.write('n');
+					break;
+				case '\t':
+					out.write('\\');
+					out.write('t');
+					break;
+				case '\f':
+					out.write('\\');
+					out.write('f');
+					break;
+				case '\r':
+					out.write('\\');
+					out.write('r');
+					break;
+				default:
+					if (ch > 0xf) {
+						out.write("\\u00" + hex(ch));
+					} else {
+						out.write("\\u000" + hex(ch));
+					}
+					break;
+				}
+			} else {
+				switch (ch) {
+				case '\\':
+					out.write('\\');
+					out.write('\\');
+					break;
+				default:
+					out.write(ch);
+					break;
+				}
+			}
+		}
+	}
+
+	private static String hex(char ch) {
+		return Integer.toHexString(ch).toUpperCase();
 	}
 
 }
