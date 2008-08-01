d265cf02db25ac12a3a8d6b3e057d566bd8d3587
apanchenk <apanchenk>
2008-08-01 09:51:01 +0000

    format until blocks

--- tests/org.eclipse.dltk.ruby.formatter.tests/scripts/blocks.rb
+++ tests/org.eclipse.dltk.ruby.formatter.tests/scripts/blocks.rb
@@ -10,6 +10,14 @@ while a < 10
 	a += 1
 end
 puts a
+==== while modifier
+		a = 1
+	a += 1 while a < 10
+puts a
+==
+a = 1
+a += 1 while a < 10
+puts a
 ==== for
 for i in 1..10
 puts i
@@ -18,3 +26,55 @@ end
 for i in 1..10
 	puts i
 end
+==== until
+a = 1
+until a >= 10
+a += 1
+end
+puts a
+==
+a = 1
+until a >= 10
+	a += 1
+end
+puts a
+==== until:
+a = 1
+until a >= 10:
+a += 1
+end
+puts a
+==
+a = 1
+until a >= 10:
+	a += 1
+end
+puts a
+==== until-do
+a = 1
+until a >= 10 do
+a += 1
+end
+puts a
+==
+a = 1
+until a >= 10 do
+	a += 1
+end
+puts a
+==== until-do-inline
+	a = 1
+	until a >= 10 do a += 1; end
+puts a
+==
+a = 1
+until a >= 10 do a += 1; end
+puts a
+==== until-modifier
+	a = 1
+	a += 1 until a >= 10
+puts a
+==
+a = 1
+a += 1 until a >= 10
+puts a
