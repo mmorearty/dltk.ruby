5a3ea8cb4fed24d50fac4544f97e796525dd6991
asobolev <asobolev>
2008-09-16 07:03:32 +0000

    * fixes

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/core/RubyContentDescriber.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/core/RubyContentDescriber.java
@@ -9,7 +9,7 @@ import org.eclipse.dltk.core.ScriptContentDescriber;
 
 public class RubyContentDescriber extends ScriptContentDescriber {
 	protected static Pattern[] header_patterns = { Pattern.compile(
-			"#!.*ruby.*", Pattern.MULTILINE) }; //$NON-NLS-1$
+			"^#!.*ruby.*", Pattern.MULTILINE) }; //$NON-NLS-1$
 
 	public int describe(Reader contents, IContentDescription description)
 			throws IOException {
