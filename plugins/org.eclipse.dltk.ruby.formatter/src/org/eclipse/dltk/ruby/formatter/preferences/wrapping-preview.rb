8fefc7ebeff021c2a9bb03a12384bc0a13bf7491
apanchenk <apanchenk>
2008-09-29 12:43:26 +0000

    preview updated

--- plugins/org.eclipse.dltk.ruby.formatter/src/org/eclipse/dltk/ruby/formatter/preferences/wrapping-preview.rb
+++ plugins/org.eclipse.dltk.ruby.formatter/src/org/eclipse/dltk/ruby/formatter/preferences/wrapping-preview.rb
@@ -1,4 +1,7 @@
 #
 # Line Wrapping preview
 #
-# Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin pretium pellentesque mauris. Nullam varius pretium odio. Phasellus lacinia dolor. Ut non nisi non massa ultricies bibendum. In felis mauris, scelerisque at, adipiscing a, mattis et, sem. Pellentesque urna diam, tincidunt nec, commodo a, aliquet et, turpis. Proin nisi nulla, placerat non, condimentum at, tincidunt sodales, leo. Mauris sit amet urna. Quisque quam nisi, lobortis blandit, egestas ut, dictum varius, purus. Vivamus euismod leo. Ut felis lorem, fermentum a, hendrerit nec, varius at, lorem. In ut sapien. 
+# This is long comment. This is long comment. This is long comment. This is long comment. This is long comment. This is long comment.
+def hello
+	puts "Hello, world"
+end
