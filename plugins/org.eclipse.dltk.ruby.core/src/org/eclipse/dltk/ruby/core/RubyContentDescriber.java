9eafaf77f109effb82ed2f14de8706d112ea468a
jgangemi <jgangemi>
2008-10-01 23:37:35 +0000

    - pulled up describe(Reader, IContentDescription) and added methods to return header and footer patterns

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/core/RubyContentDescriber.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/core/RubyContentDescriber.java
@@ -1,26 +1,14 @@
 package org.eclipse.dltk.ruby.core;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.regex.Pattern;
 
-import org.eclipse.core.runtime.content.IContentDescription;
-import org.eclipse.dltk.core.DLTKContentTypeManager;
 import org.eclipse.dltk.core.ScriptContentDescriber;
 
 public class RubyContentDescriber extends ScriptContentDescriber {
 	protected static Pattern[] header_patterns = { Pattern.compile(
 			"^#!.*ruby.*", Pattern.MULTILINE) }; //$NON-NLS-1$
 
-	public int describe(Reader contents, IContentDescription description)
-			throws IOException {
-		if (checkPatterns(contents, header_patterns, null)) {
-			if (description != null) {
-				description.setProperty(DLTKContentTypeManager.DLTK_VALID,
-						Boolean.TRUE);
-			}
-			return VALID;
-		}
-		return INDETERMINATE;
+	protected Pattern[] getHeaderPatterns() {
+		return header_patterns;
 	}
 }
