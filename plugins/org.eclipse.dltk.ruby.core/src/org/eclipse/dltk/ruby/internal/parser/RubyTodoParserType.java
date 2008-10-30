1101675f5c19ed9ab8e64179e4a5c62405141947
apanchenk <apanchenk>
2008-10-30 09:17:15 +0000

    parse tasks in block comments (=begin ... =end)

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubyTodoParserType.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/parser/RubyTodoParserType.java
@@ -15,6 +15,7 @@ import org.eclipse.dltk.compiler.task.ITodoTaskPreferences;
 import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
 import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.builder.AbstractTodoTaskBuildParticipantType;
+import org.eclipse.dltk.core.builder.IBuildParticipant;
 import org.eclipse.dltk.ruby.core.RubyPlugin;
 
 public class RubyTodoParserType extends AbstractTodoTaskBuildParticipantType {
@@ -22,5 +23,68 @@ public class RubyTodoParserType extends AbstractTodoTaskBuildParticipantType {
 	protected ITodoTaskPreferences getPreferences(IScriptProject project) {
 		return new TodoTaskPreferences(RubyPlugin.getDefault()
 				.getPluginPreferences());
+	}
+
+	private static class RubyTodoTaskParser extends TodoTaskBuildParticipant {
+
+		public RubyTodoTaskParser(ITodoTaskPreferences preferences) {
+			super(preferences);
+		}
+
+		protected void reset() {
+			super.reset();
+			blockMode = false;
+		}
+
+		private boolean blockMode;
+
+		protected int findCommentStart(char[] content, int begin, int end) {
+			if (blockMode) {
+				if (checkChars(content, begin, end, "=end")) { //$NON-NLS-1$
+					blockMode = false;
+					return -1;
+				} else {
+					for (int i = begin; i < end; ++i) {
+						if (Character.isLetterOrDigit(content[i])
+								&& (!isCheckRanges() || isValid(i))) {
+							return i;
+						}
+					}
+					return -1;
+				}
+			} else {
+				if (checkChars(content, begin, end, "=begin")) { //$NON-NLS-1$
+					blockMode = true;
+					return -1;
+				} else {
+					return super.findCommentStart(content, begin, end);
+				}
+			}
+		}
+
+		/**
+		 * @param content
+		 * @param begin
+		 * @param end
+		 * @param string
+		 * @return
+		 */
+		private boolean checkChars(char[] content, int begin, int end,
+				String substring) {
+			if (begin + substring.length() <= end) {
+				for (int i = 0; i < substring.length(); ++i) {
+					if (content[begin + i] != substring.charAt(i)) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+	}
+
+	protected IBuildParticipant getBuildParticipant(
+			ITodoTaskPreferences preferences) {
+		return new RubyTodoTaskParser(preferences);
 	}
 }
