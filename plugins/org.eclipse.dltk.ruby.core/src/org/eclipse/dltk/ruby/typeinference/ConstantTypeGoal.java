cade7ad1366db71441ba9671e740f9d2906b0143
apanchenk <apanchenk>
2009-06-11 15:28:49 +0000

    hashCode/equals() implementation

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/typeinference/ConstantTypeGoal.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/typeinference/ConstantTypeGoal.java
@@ -31,4 +31,36 @@ public class ConstantTypeGoal extends AbstractTypeGoal {
 		return offset;
 	}
 
+	/*
+	 * @see java.lang.Object#hashCode()
+	 */
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + offset;
+		return result;
+	}
+
+	/*
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConstantTypeGoal other = (ConstantTypeGoal) obj;
+		if (offset != other.offset)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
