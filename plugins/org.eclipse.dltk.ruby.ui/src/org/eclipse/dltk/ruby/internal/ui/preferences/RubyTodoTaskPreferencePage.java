a26d768c374d53e4b818f44ec594f31f7d3e773c
apanchenk <apanchenk>
2009-06-13 05:36:34 +0000

    introduce TodoTaskOptionsBlock class to simplify usage of the abstract class

--- plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/preferences/RubyTodoTaskPreferencePage.java
+++ plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/preferences/RubyTodoTaskPreferencePage.java
@@ -12,28 +12,17 @@
 package org.eclipse.dltk.ruby.internal.ui.preferences;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Preferences;
 import org.eclipse.dltk.ruby.core.RubyNature;
 import org.eclipse.dltk.ruby.core.RubyPlugin;
 import org.eclipse.dltk.ui.PreferencesAdapter;
 import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage;
 import org.eclipse.dltk.ui.preferences.AbstractOptionsBlock;
-import org.eclipse.dltk.ui.preferences.AbstractTodoTaskOptionsBlock;
-import org.eclipse.dltk.ui.preferences.PreferenceKey;
+import org.eclipse.dltk.ui.preferences.TodoTaskOptionsBlock;
 import org.eclipse.dltk.ui.util.IStatusChangeListener;
 import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 
 public class RubyTodoTaskPreferencePage extends
 		AbstractConfigurationBlockPropertyAndPreferencePage {
-
-	static final PreferenceKey CASE_SENSITIVE = AbstractTodoTaskOptionsBlock
-			.createCaseSensitiveKey(RubyPlugin.PLUGIN_ID);
-
-	static final PreferenceKey ENABLED = AbstractTodoTaskOptionsBlock
-			.createEnabledKey(RubyPlugin.PLUGIN_ID);
-
-	static final PreferenceKey TAGS = AbstractTodoTaskOptionsBlock
-			.createTagKey(RubyPlugin.PLUGIN_ID);
 
 	protected String getHelpId() {
 		return null;
@@ -43,33 +32,17 @@ public class RubyTodoTaskPreferencePage extends
 		setDescription(RubyPreferencesMessages.TodoTaskDescription);
 	}
 
-	protected Preferences getPluginPreferences() {
-		return RubyPlugin.getDefault().getPluginPreferences();
-	}
-
 	protected AbstractOptionsBlock createOptionsBlock(
 			IStatusChangeListener newStatusChangedListener, IProject project,
 			IWorkbenchPreferenceContainer container) {
-		return new AbstractTodoTaskOptionsBlock(newStatusChangedListener,
-				project, getPreferenceKeys(), container) {
-			protected PreferenceKey getTags() {
-				return TAGS;
-			}
-
-			protected PreferenceKey getEnabledKey() {
-				return ENABLED;
-			}
-
-			protected PreferenceKey getCaseSensitiveKey() {
-				return CASE_SENSITIVE;
-			}
-		};
+		return new TodoTaskOptionsBlock(newStatusChangedListener, project,
+				container, RubyPlugin.PLUGIN_ID);
 	}
 
 	protected String getNatureId() {
 		return RubyNature.NATURE_ID;
 	}
-	
+
 	protected String getProjectHelpId() {
 		return null;
 	}
@@ -87,7 +60,4 @@ public class RubyTodoTaskPreferencePage extends
 		return "org.eclipse.dltk.ruby.propertyPage.todo"; //$NON-NLS-1$
 	}
 
-	protected PreferenceKey[] getPreferenceKeys() {
-		return new PreferenceKey[] { TAGS, ENABLED, CASE_SENSITIVE };
-	}
 }
