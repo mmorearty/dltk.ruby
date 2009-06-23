99790a590748af0fb8b2323bfade99fc4fead317
apanchenk <apanchenk>
2009-06-23 06:30:38 +0000

    RubyDocScanner extends ScriptCommentScanner, so delete TodoTagRule

--- plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/text/RubyDocScanner.java
+++ plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/text/RubyDocScanner.java
@@ -12,25 +12,25 @@ package org.eclipse.dltk.ruby.internal.ui.text;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.dltk.compiler.task.ITodoTaskPreferences;
 import org.eclipse.dltk.ruby.internal.ui.text.rules.BeginOfLineRule;
-import org.eclipse.dltk.ui.text.AbstractScriptScanner;
 import org.eclipse.dltk.ui.text.IColorManager;
-import org.eclipse.dltk.ui.text.TodoTagRule;
+import org.eclipse.dltk.ui.text.ScriptCommentScanner;
 import org.eclipse.dltk.ui.text.TodoTaskPreferencesOnPreferenceStore;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.rules.WhitespaceRule;
 
-public class RubyDocScanner extends AbstractScriptScanner {
+public class RubyDocScanner extends ScriptCommentScanner {
 	private static final String[] fgTokenProperties = new String[] {
 			IRubyColorConstants.RUBY_DOC, IRubyColorConstants.RUBY_DOC_TOPIC,
 			IRubyColorConstants.RUBY_TODO_COMMENT };
 
 	public RubyDocScanner(IColorManager manager, IPreferenceStore store) {
-		super(manager, store);
-
+		super(manager, store, IRubyColorConstants.RUBY_DOC,
+				IRubyColorConstants.RUBY_TODO_COMMENT,
+				new TodoTaskPreferencesOnPreferenceStore(store), false);
 		initialize();
 	}
 
@@ -45,22 +45,38 @@ public class RubyDocScanner extends AbstractScriptScanner {
 
 		final IToken topic = getToken(IRubyColorConstants.RUBY_DOC_TOPIC);
 		final IToken other = getToken(IRubyColorConstants.RUBY_DOC);
-		final IToken todo = getToken(IRubyColorConstants.RUBY_TODO_COMMENT);
 
 		// Add generic whitespace rule.
 		rules.add(new WhitespaceRule(new RubyWhitespaceDetector()));
 		rules.add(new BeginOfLineRule(topic, '='));
-		rules.add(createTodoTagRule(todo));
+		rules.add(createTodoRule());
 
 		setDefaultReturnToken(other);
 		return rules;
 	}
 
-	private TodoTagRule createTodoTagRule(final IToken token) {
-		final ITodoTaskPreferences prefs = new TodoTaskPreferencesOnPreferenceStore(
-				getPreferenceStore());
-		return new TodoTagRule(token, prefs.getTagNames(), prefs
-				.isCaseSensitive());
+	@Override
+	protected int skipCommentChars() {
+		return 0;
+	}
+
+	/**
+	 * FIXME Standard implementation copied (Alex)
+	 */
+	@Override
+	public IToken nextToken() {
+		fTokenOffset = fOffset;
+		fColumn = UNDEFINED;
+		if (fRules != null) {
+			for (int i = 0; i < fRules.length; i++) {
+				IToken token = (fRules[i].evaluate(this));
+				if (!token.isUndefined())
+					return token;
+			}
+		}
+		if (read() == EOF)
+			return Token.EOF;
+		return fDefaultReturnToken;
 	}
 
 }
