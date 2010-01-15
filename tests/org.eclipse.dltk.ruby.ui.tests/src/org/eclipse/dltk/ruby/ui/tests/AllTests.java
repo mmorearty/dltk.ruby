51df9fdea6076da9d785fccaa7f63aea728d4169
apanchenk <apanchenk>
2010-01-15 13:31:09 +0000

    rename top level test suites

--- tests/org.eclipse.dltk.ruby.ui.tests/src/org/eclipse/dltk/ruby/ui/tests/AllTests.java
+++ tests/org.eclipse.dltk.ruby.ui.tests/src/org/eclipse/dltk/ruby/ui/tests/AllTests.java
@@ -14,7 +14,7 @@ import org.eclipse.dltk.ruby.ui.tests.text.indenting.IndentingTest;
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for org.eclipse.dltk.ruby.ui.tests");
+		TestSuite suite = new TestSuite("org.eclipse.dltk.ruby.ui.tests");
 		// $JUnit-BEGIN$
 		suite.addTestSuite(RubyAutoIndentStrategyTest.class);
 		suite.addTestSuite(PartitioningTest.class);
