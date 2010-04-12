6482f22272be464ae613b35004503164e3fd8306
apanchenk <apanchenk>
2010-04-12 13:36:16 +0000

    IMatchLocator refactoring

--- plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/core/search/RubySearchFactory.java
+++ plugins/org.eclipse.dltk.ruby.core/src/org/eclipse/dltk/ruby/internal/core/search/RubySearchFactory.java
@@ -9,22 +9,12 @@
  *******************************************************************************/
 package org.eclipse.dltk.ruby.internal.core.search;
 
-import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.dltk.core.ISearchPatternProcessor;
 import org.eclipse.dltk.core.search.AbstractSearchFactory;
-import org.eclipse.dltk.core.search.IDLTKSearchScope;
 import org.eclipse.dltk.core.search.IMatchLocatorParser;
-import org.eclipse.dltk.core.search.SearchPattern;
-import org.eclipse.dltk.core.search.SearchRequestor;
 import org.eclipse.dltk.core.search.matching.MatchLocator;
 
 public class RubySearchFactory extends AbstractSearchFactory {
-
-	public MatchLocator createMatchLocator(SearchPattern pattern,
-			SearchRequestor requestor, IDLTKSearchScope scope,
-			SubProgressMonitor monitor) {
-		return new RubyMatchLocator(pattern, requestor, scope, monitor);
-	}
 
 	public IMatchLocatorParser createMatchParser(MatchLocator locator) {
 		return new RubyMatchLocatorParser(locator);
