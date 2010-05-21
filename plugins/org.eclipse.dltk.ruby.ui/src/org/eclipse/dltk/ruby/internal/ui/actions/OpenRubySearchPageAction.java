b20d6cc9f1794afe56e5731bfff1952cae1781eb
apanchenk <apanchenk>
2010-05-21 06:42:45 +0000

    base class extracted

--- plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/actions/OpenRubySearchPageAction.java
+++ plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/actions/OpenRubySearchPageAction.java
@@ -1,45 +1,13 @@
 package org.eclipse.dltk.ruby.internal.ui.actions;
 
-import org.eclipse.dltk.ui.DLTKUIPlugin;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.search.ui.NewSearchUI;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.dltk.ui.actions.ScriptOpenSearchPageAction;
 
-public class OpenRubySearchPageAction implements IWorkbenchWindowActionDelegate {
+public class OpenRubySearchPageAction extends ScriptOpenSearchPageAction {
 
 	private static final String RUBY_SEARCH_PAGE_ID = "org.eclipse.dltk.ui.RubySearchPage"; //$NON-NLS-1$
 
-	private IWorkbenchWindow window;
-
-	public OpenRubySearchPageAction() {
-	}
-
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
-	}
-
-	public void run(IAction action) {
-		if (window == null || window.getActivePage() == null) {
-			beep();
-			return;
-		}
-
-		NewSearchUI.openSearchDialog(window, RUBY_SEARCH_PAGE_ID);
-	}
-
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
-
-	public void dispose() {
-		window = null;
-	}
-
-	protected void beep() {
-		Shell shell = DLTKUIPlugin.getActiveWorkbenchShell();
-		if (shell != null && shell.getDisplay() != null)
-			shell.getDisplay().beep();
+	@Override
+	protected String getSearchPageId() {
+		return RUBY_SEARCH_PAGE_ID;
 	}
 }
