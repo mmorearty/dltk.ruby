dbd0b9d94eaa20ccf3937ce31f255dbae50b081f
apanchenk <apanchenk>
2010-11-10 23:58:54 +0000

    simplify ScriptMergeViewer for adopters

--- plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/compare/RubyMergeViewer.java
+++ plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/compare/RubyMergeViewer.java
@@ -12,40 +12,14 @@
 package org.eclipse.dltk.ruby.internal.ui.compare;
 
 import org.eclipse.compare.CompareConfiguration;
-import org.eclipse.dltk.ruby.internal.ui.Messages;
-import org.eclipse.dltk.ruby.internal.ui.RubyUI;
+import org.eclipse.dltk.ruby.internal.ui.RubyUILanguageToolkit;
 import org.eclipse.dltk.ui.compare.ScriptMergeViewer;
-import org.eclipse.dltk.ui.text.ScriptTextTools;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 
 public class RubyMergeViewer extends ScriptMergeViewer {
 
 	public RubyMergeViewer(Composite parent, CompareConfiguration configuration) {
-		super(parent, configuration);
-	}
-
-	protected ScriptTextTools getTextTools() {
-		return RubyUI.getDefault().getTextTools();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.dltk.ruby.internal.ui.compare.ScriptMergeViewer#
-	 * getPreferenceStore()
-	 */
-	protected IPreferenceStore getPreferenceStore() {
-		return RubyUI.getDefault().getPreferenceStore();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.dltk.ui.compare.ScriptMergeViewer#getTitle()
-	 */
-	public String getTitle() {
-		return Messages.CompareTitle;
+		super(parent, configuration, RubyUILanguageToolkit.getInstance());
 	}
 
 }
